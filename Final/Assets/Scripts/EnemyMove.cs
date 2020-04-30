5fd742bff49d6f2edeb0c2ca6c02096cfb59c0d6
cjw20 <cjw20@iu.edu>
2020-04-30 15:04:06 -0400

    made enemy move to either player or house

--- Final/Assets/Scripts/EnemyMove.cs
+++ Final/Assets/Scripts/EnemyMove.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class EnemyMove : MonoBehaviour
 {
-
-
+    public Transform target;
+    public float moveSpeed = 2f;
+    
     public int health = 10;
+    
 
+
+    void FixedUpdate()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+    }
     public void TakeDamage (int damage)
     {
         health -= damage;
