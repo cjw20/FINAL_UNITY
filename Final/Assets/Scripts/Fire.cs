4ec6a227184c078604e3d96174132d9a67fdf1d7
cjw20 <cjw20@iu.edu>
2020-04-30 13:35:59 -0400

    Made fireball destroy self on impact

---
+++ Final/Assets/Scripts/Fire.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour
+{
+    void Start()
+    {
+        //Destory(gameObject, 5f);
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+    }
+}
